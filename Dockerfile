feb982d945708d8146ba25d1c85db76e355c364d
benfugate <benjamin_fugate13@yahoo.com>
2022-03-11 11:46:20 -0500

    organize files by episode number

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
 FROM ubuntu:focal
 
+ARG DEBIAN_FRONTEND=noninteractive
+ENV TZ=UTC
+
 RUN apt-get update && \
     apt-get install -y jq wget ca-certificates curl \
         mkvtoolnix mediainfo ffmpeg \
-        python3 python-is-python3 && \
+        python3 python3-pip python-is-python3 && \
     wget https://github.com/ytdl-org/youtube-dl/releases/latest/download/youtube-dl -O /usr/local/bin/youtube-dl && \
     chmod a+x /usr/local/bin/youtube-dl && \
     wget https://github.com/benibela/xidel/releases/download/Xidel_0.9.8/xidel_0.9.8-1_amd64.deb && \
-    dpkg -i xidel_0.9.8-1_amd64.deb
+    dpkg -i xidel_0.9.8-1_amd64.deb && \
+    pip3 install pycryptodome
 
 WORKDIR /usr/src/app
 RUN mkdir downloads
--- download_show.sh
+++ download_show.sh
@@ -14,16 +14,17 @@ fi
 
 ## Begin Loop..
 while read -r SEASON_NUMBER; do
-  mkdir -p downloads/season_$SEASON_NUMBER
-  cp paramount-dl.sh downloads/season_$SEASON_NUMBER
+  mkdir -p downloads/$URL_SLUG/season_$SEASON_NUMBER
+  cp paramount-dl.sh downloads/$URL_SLUG/season_$SEASON_NUMBER
 	# Now That We Know How Many Seasons We're Working With, Let's Pull The JSON For Each Season
 	JSON_DUMP=$(curl "https://www.paramountplus.com/shows/$URL_SLUG/video/xhr/episodes/page/0/size/1000/xs/0/season/$SEASON_NUMBER/" -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'Referer: https://www.paramountplus.com/shows/' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'TE: trailers')
 
 	# For Each Season We Loop Thru, We'll Extract The Episode URLs in to a TXT File
-	echo $JSON_DUMP | jq -r '.result.data[].metaData.videoPageURL' | sed 's#^#https://paramountplus.com#g' >> downloads/season_$SEASON_NUMBER/links.txt
+	echo $JSON_DUMP | jq -r '.result.data[].metaData.videoPageURL' | sed 's#^#https://paramountplus.com#g' >> downloads/$URL_SLUG/season_$SEASON_NUMBER/links.txt
+	EPISODES=`echo $JSON_DUMP | jq -r '.result.data[].episode_number'`
 
-	cd downloads/season_$SEASON_NUMBER/
-	/bin/bash paramount-dl.sh &
+	cd downloads/$URL_SLUG/season_$SEASON_NUMBER/
+	/bin/bash paramount-dl.sh $SEASON_NUMBER $EPISODES &
 	cd ../..
 done< <(seq $SEASON_COUNT)
 
