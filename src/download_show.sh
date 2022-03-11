569a908aa5a2adfe2172cc8e088e9660feefe2cc
benfugate <benjamin_fugate13@yahoo.com>
2022-03-11 15:27:46 -0500

    Reduce download output messages

--- src/download_show.sh
+++ src/download_show.sh
@@ -14,8 +14,8 @@ fi
 
 ## Begin Loop..
 while read -r SEASON_NUMBER; do
-  mkdir -p downloads/$URL_SLUG/season_$SEASON_NUMBER
-  cp paramount-dl.sh downloads/$URL_SLUG/season_$SEASON_NUMBER
+	mkdir -p downloads/$URL_SLUG/season_$SEASON_NUMBER
+	cp paramount-dl.sh downloads/$URL_SLUG/season_$SEASON_NUMBER
 	# Now That We Know How Many Seasons We're Working With, Let's Pull The JSON For Each Season
 	JSON_DUMP=$(curl "https://www.paramountplus.com/shows/$URL_SLUG/video/xhr/episodes/page/0/size/1000/xs/0/season/$SEASON_NUMBER/" -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'Referer: https://www.paramountplus.com/shows/' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'TE: trailers')
 
@@ -23,10 +23,11 @@ while read -r SEASON_NUMBER; do
 	echo $JSON_DUMP | jq -r '.result.data[].metaData.videoPageURL' | sed 's#^#https://paramountplus.com#g' >> downloads/$URL_SLUG/season_$SEASON_NUMBER/links.txt
 	EPISODES=`echo $JSON_DUMP | jq -r '.result.data[].episode_number'`
 
+	SCRIPT_HOME=$PWD
 	cd downloads/$URL_SLUG/season_$SEASON_NUMBER/
 	/bin/bash paramount-dl.sh $SEASON_NUMBER $EPISODES &
-	cd ../..
+	cd $SCRIPT_HOME
 done< <(seq $SEASON_COUNT)
 
-while pgrep -fl "paramount-dl.sh"; do sleep 6; done
+while pgrep -fl "paramount-dl.sh" >/dev/null; do sleep 6; done
 exit
